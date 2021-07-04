e647968bd815fc6240c509d75380d39258e71a65
kien beo <vutukien1995@gmail.com>
2021-07-04 17:43:13 +0700

    fix connect db

--- db.js
+++ db.js
@@ -9,7 +9,14 @@ mongoose.set('debug', true);
 // });
 // mongoose.connect('mongodb+srv://root:root95@cluster0.uh3qi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
 mongoose.connect('mongodb+srv://root:root@cluster0.tgu2z.mongodb.net/myFirstDatabase?authSource=admin&retryWrites=true&w=majority',{
-	useNewUrlParser: true
-}).catch(err => {
-	console.log(err);
-});
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true,
+  })
+  .then(() => {
+	console.log('Database connected successfully!');
+  })
+  .catch((err) => {
+	console.log('Error connecting with error code:', err);
+  });
